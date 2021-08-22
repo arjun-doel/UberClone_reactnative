b32a69ba878e121bf04b1d8474383c157ac08176
Arjun Doel <arjundoel@gmail.com>
2021-08-22 19:29:43 +0100

    config and eniroment setup with google api keys

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native'
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env';
 
 const HomeScreen = () => {
   return (
@@ -15,6 +17,13 @@ const HomeScreen = () => {
             uri: "https://links.papareact.com/gzs",
           }}
         />
+
+        <GooglePlacesAutocomplete
+          placeholder="Where From?"
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+        />
+
         <NavOptions />
       </View>
     </SafeAreaView>
