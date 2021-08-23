6e6981ea6b6faf7cf15499e0378b527f5455cbd2
Arjun Doel <arjundoel@gmail.com>
2021-08-23 13:56:01 +0100

    destination marker and route added to maps

--- components/Map.js
+++ components/Map.js
@@ -3,14 +3,28 @@ import { StyleSheet, Text, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import tw from 'tailwind-react-native-classnames';
 import { useSelector } from 'react-redux';
-import { selectOrigin } from '../slices/navSlice';
+import { selectOrigin, selectDestination } from '../slices/navSlice';
+import MapViewDirections from 'react-native-maps-directions';
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import { useEffect, useRef } from 'react';
 
 const Map = () => {
 
-  const origin = useSelector(selectOrigin)
+  const origin = useSelector(selectOrigin);
+  const destination = useSelector(selectDestination);
+  const mapRef = useRef(null)
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+
+    mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 }
+    })
+  }, [origin, destination])
 
   return (
       <MapView
+        ref={mapRef}
         style={tw`flex-1`}
         mapType="mutedStandard"
         initialRegion={{
@@ -20,6 +34,17 @@ const Map = () => {
           longitudeDelta: 0.0421,
         }}
       >
+
+        {origin && destination && (
+          <MapViewDirections 
+            origin={origin.description}
+            destination={destination.description}
+            apikey={GOOGLE_MAPS_APIKEY}
+            strokeWidth={3}
+            strokeColor="black"
+          />
+        )}
+
         {origin?.location && (
           <Marker 
             coordinate={{
@@ -31,6 +56,19 @@ const Map = () => {
             identifier="origin"
           />
         )}
+
+        {destination?.location && (
+          <Marker 
+            coordinate={{
+              latitude: destination.location.lat,
+              longitude: destination.location.lng,
+            }}
+            title="destination"
+            description={destination.description}
+            identifier="destination"
+          />
+        )}
+
       </MapView>
   )
 }
--- components/NavigationCard.js
+++ components/NavigationCard.js
@@ -1,14 +1,61 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView} from 'react-native'
+import tw from 'tailwind-react-native-classnames';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import { setDestination } from '../slices/navSlice';
+import { useDispatch } from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
 
 const NavigationCard = () => {
+  const dispatch = useDispatch(setDestination);
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>Yo</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={tw`bg-white flex-1`}>
+      <Text style={tw`text-center py-5 text-xl`}>Good Morning, Arjun</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+        <View>
+          <GooglePlacesAutocomplete 
+            styles={toInputBoxStyles}
+            placeholder={'Where to?'}
+            debounce={400}
+            fetchDetails={true}
+            enablePoweredByContainer={false}
+            onPress={(data, details = null) => {
+              dispatch(setDestination({
+                location: details.geometry.location,
+                description: data.description,
+                })
+              );
+              navigation.navigate('RideOptionCard');
+            }}
+            nearbyPlacesAPI="GooglePlacesSearch"
+            query={{
+              key: GOOGLE_MAPS_APIKEY,
+              language: 'en'
+            }}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
 export default NavigationCard
 
-const styles = StyleSheet.create({})
+const toInputBoxStyles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    paddingTop: 20,
+    flex: 0
+  },
+  textInput: {
+    backgroundColor: '#DDDDDF',
+    borderRadius: 0,
+    fontSize: 18,
+  },
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: 0,
+  }
+})
